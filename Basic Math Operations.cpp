ad4efb074435c533c9b2e08ddf15fd6ad3212a5f
adityaanil611 <112651822+adityaanil611@users.noreply.github.com>
2023-10-27 19:32:23 +0530

    Add files via upload

---
+++ Basic Math Operations.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int Var1,Var2,Var3;
+    cout<<"Enter number 1-"<<endl;
+    cin>>Var1;
+    cout<<"Enter number 2-"<<endl;
+    cin>>Var2;
+    cout<<"Sum of given inputs="<<Var1+Var2<<endl;
+    cout<<"Difference of given inputs="<<Var1-Var2<<endl;
+    cout<<"Product of given inputs="<<Var1*Var2<<endl;
+    cout<<"Division of given inputs="<<Var1/Var2<<endl;
+    return 0;
+}
